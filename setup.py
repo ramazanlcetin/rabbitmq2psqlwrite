c569f9b1f7d1a9bb617ad266c10b96458a5186ba
Ramazan Cetin <ramazancetin@mantis.com.tr>
2021-09-06 16:53:55 +0300

    setup configuration change

--- setup.py
+++ setup.py
@@ -1,23 +1,33 @@
 # coding=utf-8
 
 import setuptools
- 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
- 
+
 setuptools.setup(
-    name="rabbitmq2psqlwrite",
+    name="psql2rabbitmq",
     version="1.0.0",
     author="Ramazan ÇETİN",
     author_email="ramazan.cetin0640@gmail.com",
-    description="RabbitMQ logs to update DB row",
-    long_description=long_description,
+    description="RabbitMQ listen and read data change database",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
+    platforms="all",
+    url="https://github.com/mantis-software-company/rabbitmq2psqlwrite",
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
+        "Topic :: Internet",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+    "Topic :: Software Development :: Testing",
+        "Intended Audience :: Developers",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
     ],
-    python_requires='>=3.6',
+    install_requires=['aiopg', 'aio_pika'],
+    python_requires=">3.6.*, <4",
+    packages=['rabbitmq2psqlwrite'],
+    scripts=['bin/rabbitmq2psqlwrite']
 )
